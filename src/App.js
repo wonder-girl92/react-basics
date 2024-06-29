18b9565b79f25680c59d5b4ff8a14e9375aa0c80
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2024-06-29 15:07:28 +0300

    accordion

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Accordion from './components/accordion';
+
+function App() {
+  return (
+    <div className='App'>
+      {/* Accordion component */}
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/accordion/index.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import data from './data';
+
+export default function Accordion() {
+  const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  console.log(selected)
+  return (
+    <div className='wrapper'>
+      <div className='accordion'>
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className='item'>
+              <div onClick={() => handleSingleSelection(dataItem.id)} className='title'>
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id ? <div className='content'>{dataItem.answer}</div> : null}
+            </div>
+          ))
+        ) : (
+          <div> No data found </div>
+        )}
+      </div>
+    </div>
+  );
+}
